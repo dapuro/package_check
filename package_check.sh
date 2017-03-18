d26296f020e35eb6daa3388a56a9d92dcb44ca30
dapuro <dapuro@riseup.net>
2017-03-18 21:06:57 +0100

    Fixes result printing
    indention and colour

--- package_check.sh
+++ package_check.sh
@@ -813,13 +813,14 @@ _print_test_result() {
   local -r result=$1
   local -r title=$2
 
-  ECHO_FORMAT "${title}: "
+  printf "%28s" "${title}:"
+
 	if [[ $result -eq 1 ]]; then
-		ECHO_FORMAT "\t\t\tSUCCESS\n" "lgreen"
+		ECHO_FORMAT "\tSUCCESS\n" "lgreen"
 	elif [[ $result -eq -1 ]]; then
-		ECHO_FORMAT "\t\t\tFAIL\n" "lred"
+		ECHO_FORMAT "\tFAIL\n" "lred"
 	else
-		ECHO_FORMAT "\t\t\tNot evaluated.\n" "white"
+		ECHO_FORMAT "\tNot evaluated.\n" "white"
 	fi
 }
 
@@ -831,30 +832,35 @@ _print_test_results() {
   declare -A results
   local result=0
   local title=""
+  local value=""
+  local i=-1
 
-  results["Package Linter"]=$GLOBAL_LINTER
-  results["Installation"]=$GLOBAL_CHECK_SETUP
-  results["Removal"]=$GLOBAL_CHECK_REMOVE
-  results["Installation on sub-path"]=$GLOBAL_CHECK_SUB_DIR
-  results["Removal from sub-path"]=$GLOBAL_CHECK_REMOVE_SUBDIR
-  results["Installation on root-path"]=$GLOBAL_CHECK_ROOT
-  results["Removal from root-path"]=$GLOBAL_CHECK_REMOVE_ROOT
-  results["Upgrade"]=$GLOBAL_CHECK_UPGRADE
-  results["Private installation"]=$GLOBAL_CHECK_PRIVATE
-  results["Public installation"]=$GLOBAL_CHECK_PUBLIC
-  results["Multi-instance installation"]=$GLOBAL_CHECK_MULTI_INSTANCE
-  results["Bad user"]=$GLOBAL_CHECK_ADMIN
-  results["Wrong domain"]=$GLOBAL_CHECK_DOMAIN
-  results["Wrong path"]=$GLOBAL_CHECK_PATH
-  results["Port already in use"]=$GLOBAL_CHECK_PORT
-  results["Backup"]=$GLOBAL_CHECK_BACKUP
-  results["Restore"]=$GLOBAL_CHECK_RESTORE
+  results[1]="Package Linter:$GLOBAL_LINTER"
+  results[2]="Installation:$GLOBAL_CHECK_SETUP"
+  results[3]="Removal:$GLOBAL_CHECK_REMOVE"
+  results[4]="Installation on sub-path:$GLOBAL_CHECK_SUB_DIR"
+  results[5]="Removal from sub-path:$GLOBAL_CHECK_REMOVE_SUBDIR"
+  results[6]="Installation on root-path:$GLOBAL_CHECK_ROOT"
+  results[7]="Removal from root-path:$GLOBAL_CHECK_REMOVE_ROOT"
+  results[8]="Upgrade:$GLOBAL_CHECK_UPGRADE"
+  results[9]="Private installation:$GLOBAL_CHECK_PRIVATE"
+  results[10]="Public installation:$GLOBAL_CHECK_PUBLIC"
+  results[11]="Multi-instance installation:$GLOBAL_CHECK_MULTI_INSTANCE"
+  results[12]="Bad user:$GLOBAL_CHECK_ADMIN"
+  results[13]="Wrong domain:$GLOBAL_CHECK_DOMAIN"
+  results[14]="Wrong path:$GLOBAL_CHECK_PATH"
+  results[15]="Port already in use:$GLOBAL_CHECK_PORT"
+  results[16]="Backup:$GLOBAL_CHECK_BACKUP"
+  results[17]="Restore:$GLOBAL_CHECK_RESTORE"
 
   ECHO_FORMAT "\n\n"
 
-  for title in "${!results[@]}"; do
-    result=${results["${title}"]}
-    _print_test_result "$result" "$title"
+  for i in {1..17}; do
+    result="${results[$i]}"
+    title=$( echo $result | cut -d : -f1 )
+    value=$( echo $result | cut -d : -f2 )
+
+    _print_test_result "$value" "$title"
   done
 }
 
@@ -864,7 +870,11 @@ _print_test_results_summary() {
   local typo_note=""
   local smiley=""
 
-	ECHO_FORMAT "\t\t    Results: $note/$tnote - " "white" "bold"
+  ECHO_FORMAT "\n"
+
+  printf '\e[1m\e[97m%28s\e[0m' "Results:"
+
+	ECHO_FORMAT "\t$note/$tnote - " "white" "bold"
 
 	if [[ $note -gt 0 ]]; then
 		note=$(( note * 20 / tnote ))
@@ -892,12 +902,22 @@ _print_test_results_summary() {
   fi
 
 	ECHO_FORMAT "$note/20 $smiley\n" "$color_note" "$typo_note"
-	ECHO_FORMAT "\t   Test set: $tnote/21\n\n" "white" "bold"
 
-	ECHO_FORMAT "Quality level of application: $level\n" "white" "bold"
+  printf '\e[1m\e[97m%28s\e[0m' "Test Set:"
+	ECHO_FORMAT "\t$tnote/21\n\n" "white" "bold"
+
+  printf "%28s" "Application quality level:"
+
+	ECHO_FORMAT "\t$level\n" "white" "bold"
+
+  ECHO_FORMAT "\n"
+
 	for i in {1..10}
 	do
-		ECHO_FORMAT "\t   Level $i: "
+		printf "%28s" "Level $i:"
+
+    ECHO_FORMAT "\t"
+
 		if [[ ${level[i]} == "na" ]]; then
 			ECHO_FORMAT "N/A\n"
 		elif [[ ${level[i]} -ge 1 ]]; then
@@ -906,6 +926,8 @@ _print_test_results_summary() {
 			ECHO_FORMAT "0\n"
 		fi
 	done
+
+  ECHO_FORMAT "\n"
 }
 
 TEST_RESULTS() {
